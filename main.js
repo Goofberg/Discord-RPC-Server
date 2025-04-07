b2a3024d3cf9b611e29140fd70b52c4201d4e8f0
Goofberg <146931234+Goofberg@users.noreply.github.com>
2025-04-08 01:32:20 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,41 +1,45 @@
 const express = require("express");
-const { Client, GatewayIntentBits } = require("discord.js");
 const rateLimit = require("express-rate-limit");
 const WebSocket = require('ws');
-
+const { Client, GatewayIntentBits } = require("discord.js");
 
 const app = express();
 app.set("trust proxy", 1);
-const port = process.env.PORT;
+const port = process.env.PORT || 10000;
+const CHANNEL_ID = "1358469943510962343";
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+});
+client.once("ready", () => {
+  console.log(`🤖 Bot is online as ${client.user.tag}`);
+});
+
 
 const wss = new WebSocket.Server({ noServer: true });
-
-const CHANNEL_ID = "1358469943510962343";
 const AUTH_TOKEN = "9183617-project-aegis";
 const clients = new Set();
 
-
-wss.on("connection", (ws, req) => {
+wss.on('connection', (ws) => {
+  console.log('A new WebSocket client connected!');
   clients.add(ws);
-  ws.send(JSON.stringify({ message: "Connected to WebSocket server!" }));
+  ws.send(JSON.stringify({ message: 'Connected to WebSocket server!' }));
 
-  ws.on("close", () => {
+  ws.on('message', (message) => {
+    console.log('Received:', message);
+  });
+
+  ws.on('close', () => {
+    console.log('WebSocket client disconnected!');
     clients.delete(ws);
   });
+
   ws.on("error", (err) => {
     console.error("⚠️ WebSocket client error:", err.message);
     clients.delete(ws);
     ws.terminate(); 
   });
 });
-
-const client = new Client({
-   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
- });
- 
- client.once("ready", () => {
-   console.log(`🤖 Bot is online as ${client.user.tag}`);
- });
 
 const jumpscareLimiter = rateLimit({
   windowMs: 20 * 1000,
@@ -70,15 +74,15 @@ app.get("/jumpscare", jumpscareLimiter, async (req, res) => {
   }
 });
 
-
 app.server = app.listen(port, () => {
   console.log(`🖥️ Local API running at http://localhost:${port}/jumpscare`);
   console.log(`🔧 WebSocket server running on ws://localhost:${port}/api/ws?token=${AUTH_TOKEN}`);
 });
+
 app.server.on('upgrade', (request, socket, head) => {
   const url = new URL(request.url, `http://${request.headers.host}`);
   const path = url.pathname;
-  
+
   const token = url.searchParams.get("token");
   console.log(`there a trying connection, ${token}, AP: ${token === AUTH_TOKEN}`)
   if (path === '/api/ws' && token === AUTH_TOKEN) {
